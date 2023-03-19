9c52287ce7293e8a3fcacd216cde7961b9727228
Bahar Alirezaei <b.alirezaei@sadad.co.ir>
2023-03-19 22:34:10 +0330

    bug

--- LoanWithUs.ViewModel/AdminRegisterSupporterVm.cs
+++ LoanWithUs.ViewModel/AdminRegisterSupporterVm.cs
@@ -5,7 +5,7 @@ namespace LoanWithUs.ViewModel
     public class AdminRegisterSupporterVm
     {
         public string NationalCode { get; set; }
-        public MobileNumber MobileNumber { get; set; }
+        public string MobileNumber { get; set; }
     }
 
     public class AdminRegisteredSupporterVm: PagingContractVm
